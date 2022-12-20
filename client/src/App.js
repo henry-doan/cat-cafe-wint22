f7497113829127d80605db6183fab0166236d33e
Henry Doan <henrydoan96@gmail.com>
2022-12-19 20:29:25 -0700

    auth done

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,28 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Nomatch from './components/shared/Nomatch';
 import MainNavbar from './components/shared/MainNavbar';
+import FetchUser from './components/auth/FetchUser';
+import Cats from './components/cats/Cats';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 const App = () => (
   <>
     <MainNavbar />
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/*' element={<Nomatch />} />
-      </Routes>
-    </>
+    <FetchUser>
+      <>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/' element={<ProtectedRoute />}>
+            {/* only routes or pages that can be access when login */}
+            <Route path='/cats' element={<Cats />} />
+
+          </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/*' element={<Nomatch />} />
+        </Routes>
+      </>
+    </FetchUser>
   </>
 )
 
---
+++ client/src/components/cats/Cats.js
@@ -0,0 +1,10 @@
+
+const Cats = () => {
+  return (
+    <>
+      <h1>Cats</h1>
+    </>
+  )
+}
+
+export default Cats;
